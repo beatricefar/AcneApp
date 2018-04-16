bf30993e725773dce986e15428bf60c9fdd6a124
beatricefar <beatricefarias.lt@gmail.com>
2018-04-16 22:03:34 +0300

    Added a displayDatabaseInfo method to display info in Home Activity using cursor.

--- app/src/main/java/com/beatricefarias/acneapp/HomeActivity.java
+++ app/src/main/java/com/beatricefarias/acneapp/HomeActivity.java
@@ -1,12 +1,17 @@
 package com.beatricefarias.acneapp;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.widget.TextView;
+
+import com.beatricefarias.acneapp.Database.DataContract.MealEntry;
+import com.beatricefarias.acneapp.Database.DataDbHelper;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,6 +23,7 @@ public class HomeActivity extends AppCompatActivity {
     FloatingActionButton addMeal;
 
     private Intent editorActivity;
+    private DataDbHelper dataDbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +32,64 @@ public class HomeActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setTitle(R.string.home_activity);
 
+        dataDbHelper = new DataDbHelper(this);
+
+        displayDatabaseInfo();
     }
 
     @OnClick(R.id.action_add_meal)
     public void addMeal(FloatingActionButton addMeal) {
         editorActivity = new Intent(HomeActivity.this, EditorActivity.class);
         startActivity(editorActivity);
+    }
+
+    /**
+     * A method to display info from database.
+     * In this case, it will display food name and category of current day,
+     * where meal type is set to breakfast.
+     */
+    private void displayDatabaseInfo() {
+        SQLiteDatabase db = dataDbHelper.getReadableDatabase();
+        int today = new Dates().getTodayDate();
+
+        String[] projection = {
+                MealEntry.COLUMN_FOOD,
+                MealEntry.COLUMN_FOOD_CATEGORY
+        };
+
+        String selection = MealEntry.COLUMN_MEAL_DATE + " = ?";
+        String selectionArgs[] = { Integer.toString(today) };
+
+        String sortOrder = MealEntry.COLUMN_FOOD + " DESC";
+
+        Cursor cursor = db.query(
+                MealEntry.TABLE_NAME,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                sortOrder
+        );
+
+        TextView displayDb = (TextView) findViewById(R.id.display_db_textview);
+
+        try {
+            displayDb.setText("Number of rows in meal database table (breakfast): " + cursor.getCount() + "\n");
+            displayDb.append(MealEntry.COLUMN_FOOD + " " + MealEntry.COLUMN_FOOD_CATEGORY + "\n");
+
+            int foodColumnIndex = cursor.getColumnIndex(MealEntry.COLUMN_FOOD);
+            int foodCategoryColumnIndex = cursor.getColumnIndex(MealEntry.COLUMN_FOOD_CATEGORY);
+
+            while (cursor.moveToNext()) {
+                String currentFood = cursor.getString(foodColumnIndex);
+                String currentFoodCategory = cursor.getString(foodCategoryColumnIndex);
+
+                displayDb.append(("\n" + currentFood + " - " + currentFoodCategory));
+            }
+        } finally {
+            cursor.close();
+        }
     }
 
     /**
